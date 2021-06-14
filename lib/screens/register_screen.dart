741137290a0b0cea2b3c9df2202d519c4abbfd81
AdrielRA <36385005+AdrielRA@users.noreply.github.com>
2021-06-14 10:47:23 -0300

    :art: #00011 Adiciona positioned tela login

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -6,10 +6,37 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Navegano"),
-        ],
+      body: SafeArea(
+        child: Stack(clipBehavior: Clip.none, children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Image(
+              image: AssetImage('lib/assets/images/01.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            top: 175,
+            height: 50,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(
+                child: Text(
+                  "CADASTRE-SE",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
