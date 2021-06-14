38aea5125f691e00170d2d000e07ceee73b0f392
“LucasRobert123” <lucasrobert994@gmail.com>
2021-06-14 14:37:30 -0300

    :construction: #000015 Telas de Menu e Carrinho feitas a interface

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,16 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:na_vegano/widgets/button_widget.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+  itemCart() {
+    return Container(
+      margin: EdgeInsets.all(8),
+      height: 105,
+      child: Row(
         children: [
-          Text("Navegano"),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: Image(
+              width: 100,
+              height: 100,
+              image: AssetImage('lib/assets/images/01.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Legume 1',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text('R\$ 10.00'),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.add_circle,
+                      size: 30,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Text('2'),
+                  IconButton(
+                    icon: Icon(Icons.remove_circle, size: 30),
+                    onPressed: () {},
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Image(
+              image: AssetImage('lib/assets/images/01.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            transform: Matrix4.translationValues(0.0, -50.0, 0.0),
+            child: Container(
+              alignment: Alignment.center,
+              height: 75,
+              child: Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Text(
+                  "CARRINHO",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              transform: Matrix4.translationValues(0, -55, 0),
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return itemCart();
+                },
+              ),
+            ),
+          ),
+          Container(
+            transform: Matrix4.translationValues(0, -20, 0),
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Button(
+              text: "FINALIZAR COMPRA",
+              onPress: () => Navigator.pushNamed(context, '/menu'),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 class MenuScreen extends StatelessWidget {
   const MenuScreen({Key key}) : super(key: key);
 
+  cardItemMenu(context) {
+    return GestureDetector(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(4))),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Image(
+                image: AssetImage('lib/assets/images/01.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'R\$ 5.00',
+                  style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.6)),
+                ),
+                Text(
+                  'Legume 1',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      onTap: () {
+        Navigator.pushNamed(context, '/cart');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,11 +66,14 @@ class MenuScreen extends StatelessWidget {
             transform: Matrix4.translationValues(0.0, -50.0, 0.0),
             child: Column(
               children: [
-                Text(
-                  "Na’Vegano",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w900,
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
+                  child: Text(
+                    "Na’Vegano",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
                 ),
                 Text(
@@ -41,6 +84,22 @@ class MenuScreen extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              transform: Matrix4.translationValues(0, -25, 0),
+              child: GridView.count(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: [
+                  cardItemMenu(context),
+                  cardItemMenu(context),
+                  cardItemMenu(context),
+                ],
+              ),
             ),
           ),
         ],
