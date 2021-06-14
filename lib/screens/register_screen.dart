be49cb314216356101778d4d4aec02c6318d79fa
AdrielRA <36385005+AdrielRA@users.noreply.github.com>
2021-06-14 11:11:16 -0300

    :construction: #00012 Estruturação interface register_screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:na_vegano/widgets/button_widget.dart';
+import 'package:na_vegano/widgets/input_widget.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key key}) : super(key: key);
@@ -7,36 +9,74 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(clipBehavior: Clip.none, children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-            child: Image(
-              image: AssetImage('lib/assets/images/01.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned(
-            top: 175,
-            height: 50,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Theme.of(context).scaffoldBackgroundColor,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Center(
-                child: Text(
-                  "CADASTRE-SE",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w900,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    child: Image(
+                      image: AssetImage('lib/assets/images/01.jpg'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
+                  Positioned(
+                    top: 175,
+                    height: 50,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Container(),
+                    ),
+                  ),
+                  Positioned(
+                    top: 182.5,
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(
+                      "CADASTRE-SE",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    Input(text: "Nome"),
+                    Input(text: "E-mail"),
+                    Input(
+                      text: "Senha",
+                      obscureText: true,
+                    ),
+                    Input(
+                      text: "Confirmar senha",
+                      obscureText: true,
+                    ),
+                    Button(
+                      text: "Cadastrar",
+                      onPress: () => Navigator.pushNamed(context, '/login'),
+                    )
+                  ],
                 ),
               ),
-            ),
+            ],
           ),
-        ]),
+        ),
       ),
     );
   }
